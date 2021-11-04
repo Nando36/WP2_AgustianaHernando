0532de1aec0c8988f291acf6ff4a082071f0d06f
Agustiana Hernando <agustianhernando@gmail.com>
2021-11-05 03:21:16 +0700

    Menambahkan Folder

---
+++ codeigniter/application/controllers/Home.php
@@ -0,0 +1,9 @@
+<?php
+
+    class Home extends CI_Controller{
+        public function index(){
+            echo "<h1>Perkenalkan</h1>";
+            echo "Nama Saya Agustiana Hernando
+                Saya tinggal di subang";
+        }
+    }
